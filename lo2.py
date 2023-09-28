b451c595011349f97e0a2dc1f971a3058b8e0779
dev-com2020 <kwp@tlen.pl>
2023-09-28 14:30:18 +0200

    14:30

--- lo2.py
+++ lo2.py
@@ -1,10 +1,48 @@
 import logging
 
 # logging.basicConfig(level=logging.ERROR)
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(filename='app.log', filemode='w', format='%(process)d - %(levelname)s - %(message)s')
+# logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(process)d - %(levelname)s - %(message)s')
+logging.basicConfig(filename='app.log',
+                    filemode='a',
+                    level=logging.DEBUG,
+                    format='%(asctime)s - %(process)d - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S')
 
-logging.debug('To jest informacja z debug')
-logging.info('To jest informacja z info')
-logging.warning('To jest informacja z warning')
-logging.error('To jest informacja z error')
-logging.critical('To jest informacja z critical')
+lista = [34, 43, 22, 566, 1, 33]
+logging.debug(f'To jest informacja z debug {lista}')
+
+
+a = 5
+b = 2
+
+try:
+    c = a / b
+except Exception as e:
+    logging.exception('To jest informacja z error')
+
+
+valid_data = [{},
+              {'name': 'Dawid', 'age': '25'},
+              {'name': 'Marcin', 'age': '23'}]
+
+
+def check(users,age):
+    licznik = 0
+    for user in users:
+        try:
+            if int(user['age']) < age:
+                licznik += 1
+        except KeyError:
+            logging.exception(f'niepoprawne dane {user}')
+        except ValueError:
+            logging.warning(f'niepoprawny wiek {user}')
+
+check(valid_data, 15)
+
+
+
+# logging.info('To jest informacja z info')
+# logging.warning('To jest informacja z warning')
+# logging.critical('To jest informacja z critical')
