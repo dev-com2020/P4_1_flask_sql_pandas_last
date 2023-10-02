948dcf089566b75166437e7b8184bcafbc9fec19
dev-com2020 <kwp@tlen.pl>
2023-10-02 14:32:44 +0200

    2.10 14:32

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -3,6 +3,16 @@ from my_app.hello.views import hello
 from markupsafe import Markup
 from my_app.product.views import product_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.register_blueprint(hello)
 app.register_blueprint(product_blueprint)
+
+
+# class momentjs:
+#     def __init__(self, timestamp):
+#         self.timestamp = timestamp
+#
+# def calendar(self):
+#     return self.render("calendar()")
+#
+# app.jinja_env.globals['momentjs'] = momentjs
