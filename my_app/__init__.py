3b46f4edf270710d60fab9fc998955361da25c91
dev-com2020 <kwp@tlen.pl>
2023-10-02 14:44:46 +0200

    2.10 14:44

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -8,11 +8,20 @@ app.register_blueprint(hello)
 app.register_blueprint(product_blueprint)
 
 
-# class momentjs:
-#     def __init__(self, timestamp):
-#         self.timestamp = timestamp
-#
-# def calendar(self):
-#     return self.render("calendar()")
-#
-# app.jinja_env.globals['momentjs'] = momentjs
+class momentjs:
+    def __init__(self, timestamp):
+        self.timestamp = timestamp
+
+    def render(self, format):
+        return Markup(
+            "<script>\ndocument.write(moment(\"%s\").%s);\n</script>" % (
+                self.timestamp.strftime("%Y-%m-%dT%H:%M:%S"), format
+            )
+        )
+    def format(self, fmt):
+        return self.render("format(\"%s\")" % fmt)
+
+    def calendar(self):
+        return self.render("calendar()")
+
+app.jinja_env.globals['momentjs'] = momentjs
