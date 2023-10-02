0a9b0d968e308b56e3d4e04bbad9fccfecb41bd9
dev-com2020 <kwp@tlen.pl>
2023-10-02 12:23:06 +0200

    2.10 12:23

--- my_app/hello/views.py
+++ my_app/hello/views.py
@@ -3,13 +3,24 @@ from my_app.hello.models import MESSAGES
 
 hello = Blueprint('hello', __name__)
 
+
 @hello.route('/')
+@hello.route('/hello')
 def hello_world():
     return MESSAGES['default']
+
 
 @hello.route('/great')
 def hello_world2():
     return MESSAGES['great']
 
 
+@hello.route('/show/<key>')
+def get_message(key):
+    return MESSAGES.get(key) or f"{key} nie znaleziony!"
 
+
+@hello.route('/add/<key>/<message>')
+def add_or_update(key, message):
+    MESSAGES[key] = message
+    return f"{key} jest zaktualizowany!"
