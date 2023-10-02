2e18722dcdf80a74f3faf033e3984f4812c020fd
dev-com2020 <kwp@tlen.pl>
2023-10-02 13:37:22 +0200

    2.10 13:37

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 from my_app.hello.views import hello
+from markupsafe import Markup
 
 app = Flask(__name__)
 app.register_blueprint(hello)
+app.register_blueprint(product_blueprint)
